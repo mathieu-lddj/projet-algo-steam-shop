d54e11e6c8a693eebbdfcf30428ebc1eea10b933
mathieu-lddj <mathieu.le-du-de-jesus@epita.fr>
2025-01-12 23:38:53 +0100

    commit display v1

--- env/store/views.py
+++ env/store/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from store.models import Product
 # Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return HttpResponse(render(request, 'store/index.html', context={"products": products}))
