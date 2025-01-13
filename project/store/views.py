2a50780f56f3af6b1d8df1bdd95a65da338c05a8
mathieu-lddj <mathieu.le-du-de-jesus@epita.fr>
2025-01-13 13:58:09 +0100

    tt

--- project/store/views.py
+++ project/store/views.py
@@ -4,4 +4,7 @@ from store.models import Product
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    return HttpResponse(render(request, 'store/index.html', context={"products": products}))
+    return render(request, 'store/index.html', context={"products": products})
+
+def product_detail(request, slug):
+    return HttpResponse(slug)
