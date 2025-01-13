376ec50eb35695fe41e2fe15cd08044ffe576fc7
mathieu-lddj <mathieu.le-du-de-jesus@epita.fr>
2025-01-13 14:45:43 +0100

    linkage first part

--- project/store/views.py
+++ project/store/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from store.models import Product
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 def index(request):
     products = Product.objects.all()
     return render(request, 'store/index.html', context={"products": products})
 
 def product_detail(request, slug):
-    return HttpResponse(slug)
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'store/detail.html', context={"product": product})
