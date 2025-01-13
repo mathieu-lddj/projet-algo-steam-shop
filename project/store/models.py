dd6920dd29a8524cf16df0cc17f06bdb7b4df057
mathieu-lddj <mathieu.le-du-de-jesus@epita.fr>
2025-01-13 11:39:53 +0100

    migrate

--- project/store/models.py
+++ project/store/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128)
     price = models.FloatField(default=0.0)
     description = models.TextField(blank=True)
     thumbnail = models.ImageField(upload_to="products",blank=True,null=True)
