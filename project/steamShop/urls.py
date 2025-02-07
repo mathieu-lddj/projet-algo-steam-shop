3436cdb8de07f8fc0894b47275fa6b65ed6f7d01
mathieu-lddj <mathieu.le-du-de-jesus@epita.fr>
2025-02-07 13:05:49 +0100

    yup

--- project/steamShop/urls.py
+++ project/steamShop/urls.py
@@ -19,9 +19,11 @@ from django.urls import path
 from store.views import index, product_detail
 from django.conf.urls.static import static
 from steamShop import settings
+from accounts.views import signup
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('signup/', signup, name='signup'),
     path('product/<str:slug>', product_detail, name="product"),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
