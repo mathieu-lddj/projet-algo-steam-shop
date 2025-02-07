5644d8c7dbaa8818163ea18db47963dbb0965439
mathieu-lddj <mathieu.le-du-de-jesus@epita.fr>
2025-02-07 14:20:13 +0100

    signup, login, logout added

--- project/steamShop/urls.py
+++ project/steamShop/urls.py
@@ -19,11 +19,13 @@ from django.urls import path
 from store.views import index, product_detail
 from django.conf.urls.static import static
 from steamShop import settings
-from accounts.views import signup
+from accounts.views import signup,login_user, logout_user
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('signup/', signup, name='signup'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('product/<str:slug>', product_detail, name="product"),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
